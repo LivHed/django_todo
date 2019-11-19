e55497624330da53ff4fa8df32961a1fcc681356
Ubuntu <ubuntu@ip-172-31-94-239.ec2.internal>
2019-11-19 14:13:18 +0000

    Add url to the edit function, create form in html and edit button

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -17,4 +17,10 @@ def create_an_item(request):
     else:
         form = ItemForm()
         
+    return render(request, 'item_form.html', {'form': form})
+    
+
+def edit_an_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+    form = ItemForm(instance=item)
     return render(request, 'item_form.html', {'form': form})
