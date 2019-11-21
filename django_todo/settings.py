aa5d9744fa4760f6836177af3e0a9600fa8d849f
Ubuntu <ubuntu@ip-172-31-94-239.ec2.internal>
2019-11-21 12:55:28 +0000

    Add new databases connection, a dictionary for url, comment out the old one, and import

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse("postgres://ihzecwfpbvffcl:f5d7e5cbecb3fde629234bd82335fd32225abe25e0104bca0c99d791706c3b16@ec2-54-247-92-167.eu-west-1.compute.amazonaws.com:5432/d74jquuovcgapa")
 }
 
 
